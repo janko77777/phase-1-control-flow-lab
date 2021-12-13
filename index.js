a6657c7caf0fe8e19ea28e43a010e21fd186906e
Steven Jankowski <steven.jankowski1@gmail.com>
2021-12-13 21:11:54 +0000

    commit tree 46277c4c3c4547bcc4a23b4e29d2cc0ec7e19ddf

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let message
+  if (feet > 2500) {
+    message = "No can do."
+    } 
+  else if (feet > 2000) {
+    message = "I will gladly take your thirty bucks."
+    }
+  else if (feet <= 400) {
+    message = "This one is on me!"
+  }
+return message
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+let message2
+city === "NYC" ? (message2 = "Ok, sounds good.") : (message2 = "No go.");
+return message2
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip){
+switch (tip) {
+  case "generous":
+    return "Thank you so much."
+  case "not as generous":
+    return "Thank you.";
+  default:
+    return "Bye.";
+}
 }
